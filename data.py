69b71eec431339f740e6d09f3d37663a41f10ba9
daniellin-1989 <DAniell619@nycstudents.net>
2024-02-05 14:00:11 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,17 @@
+""" a = ("Chan" "is" "cheating" "on" "Maria" "with" "Jason")
+print(a) """
+
+#Boolean
+""" is_Chan_cheating_on_Maria = input("Is Chan cheating on Maria with Jason?")
+if is_Chan_cheating_on_Maria == "Yes":
+    print("Yes he is")
+elif is_Chan_cheating_on_Maria == "No":
+    print("You are wrong")
+else:
+    print("IT IS A YES OR NO QUESTION!") """
+
+odd_or_even = int (input ("Enter any number to test whether it is odd or even:") )
+if ((odd_or_even) % 2) == 0: 
+    print ("The number is even")
+else:
+    print ("The provided number is odd")
